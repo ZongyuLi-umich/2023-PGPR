8c012b917f92ed225b05c3cb70b5bc82a14f7d6d
Zongyu Li <54018575+ZongyuLi-umich@users.noreply.github.com>
2024-04-21 14:53:45 -0400

    Delete data directory

--- data/Set12/resizeimg.m
+++
@@ -1,5 +0,0 @@
-
-img_name = '12';
-img = imread(sprintf('/Users/xuxiaojian/Downloads/set12/%s.png', img_name));
-img = imresize(img,[256 256]);
-imwrite(img, sprintf('/Users/xuxiaojian/Downloads/set11/%s.png', img_name), 'png')
